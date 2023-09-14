082931aabfd6075c6f54969db885e80c1f5109cb
arturmihk <115464760+arturmihk@users.noreply.github.com>
2023-09-14 09:36:02 +0300

    Add files via upload

--- ArturJava/iseseisev.java
+++ ArturJava/iseseisev.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class iseseisev {
     public static void main(String[] args) {
@@ -6,7 +7,7 @@ public class iseseisev {
         u13();
         //u15();
         //u23();
-        //u24();
+        u24();
         }
 
         static void u9() {
@@ -29,11 +30,64 @@ public class iseseisev {
         
         sisestus.close();
     }
-}
-        //static void u15();
-        //static void u23();
-        //static void u24();
 
+        //static void u15() {
+        //}
+        //static void u23() {
+        
+        static void u24() {
+            Random rand = new Random();
+            int arv = rand.nextInt(100);
+            int arvamisi = 0;
+
+            Scanner mangukorrad = new Scanner(System.in);
+            System.out.print("Kas soovite mängida 3 korda (1) või lõpmatuseni (2)?:");
+            int korrad = mangukorrad.nextInt();
+
+            Scanner sisestus = new Scanner(System.in);
+            System.out.print("Arva ära arv 1-100: ");
+            int arvamus = sisestus.nextInt();
+            
+            if (korrad == 1) {
+                while (arvamisi < 3) {
+                    if (arvamus == arv) {
+                        System.out.println("Arvasid ära!");
+                        break;
+                    } else if (arvamus < arv) {
+                        System.out.println("Arv on suurem");
+                        arvamisi++;
+                        System.out.print("Arva uuesti: ");
+                        arvamus = sisestus.nextInt();
+                    } else if (arvamus > arv) {
+                        System.out.println("Arv on väiksem");
+                        arvamisi++;
+                        System.out.print("Arva uuesti: ");
+                        arvamus = sisestus.nextInt();
+                    }
+                }
+        }   else
+                while (arvamisi >= 0) {
+                    if (arvamus == arv) {
+                        System.out.println("Arvasid ära!");
+                        break;
+                }   else if (arvamus < arv) {
+                        System.out.println("Arv on suurem");
+                        arvamisi++;
+                        System.out.print("Arva uuesti: ");
+                        arvamus = sisestus.nextInt();
+                }   else if (arvamus > arv) {
+                        System.out.println("Arv on väiksem");
+                        arvamisi++;
+                        System.out.print("Arva uuesti: ");
+                        arvamus = sisestus.nextInt();
+                }
+            }
+            sisestus.close();
+            mangukorrad.close();
+        }
+
+        
+}
 
 
 
