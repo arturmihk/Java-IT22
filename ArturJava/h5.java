c560606fc60b6138b07ac4239bd9dec7f9af4dda
arturmihk <115464760+arturmihk@users.noreply.github.com>
2023-09-21 14:21:39 +0300

    Add files via upload

--- ArturJava/h5.java
+++ ArturJava/h5.java
@@ -1,21 +1,27 @@
 import java.util.Scanner;
 
 public class h5 {
-    public static void main(string[] args) {
-        jagamine();
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            jagamine();
+        }
     }
 
-    static void jagamine() { 
-            private static Scanner scanner = new Scanner(System.in);
-            System.out.print("Sisesta esimene arv: ");
-            int arv1 = scanner.nextInt();
-            System.out.print("Sisesta teine arv: ");
-            int arv2 = scanner.nextInt();
-            while(arv1>=0 && arv2>=0){
-            System.out.println(arv1 + arv2);
-            arv++;
+    static void jagamine() {
+        System.out.print("Sisesta esimene arv: ");
+        int arv1 = scanner.nextInt();
+        System.out.print("Sisesta teine arv: ");
+        int arv2 = scanner.nextInt();
+
+        if (arv1 >= 0 && arv2 > 0) {
+            double jagatis = (double) arv1 / arv2;
+            System.out.println(String.format("%d / %d = %f", arv1, arv2, jagatis));
+            return;
+        } else {
+            System.out.println("Arvud peavad olema positiivsed!");
         }
 
     }
-
 }
--- ArturJava/iseseisev.java
+++ ArturJava/iseseisev.java
@@ -2,97 +2,175 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class iseseisev {
+
+    private static Scanner sisestus = new Scanner(System.in);
+
     public static void main(String[] args) {
         u9();
         u13();
-        //u15();
-        //u23();
+        System.out.print("Sisestage isikukood: ");
+        String isikukood = sisestus.nextLine();
+        u15(isikukood);
+        u23();
         u24();
-        }
-
-        static void u9() {
-            for (int i = 0; i < 66; i++) {
-                System.out.println(i + 1);
-            }
-        }
-        static void u13() {
-            Scanner sisestus = new Scanner(System.in);
-        
-            System.out.print("Sisestage kolmnurga kõrgus: ");
-            int korgus = sisestus.nextInt();
-        
-         for (int i = 1; i <= korgus; i++) {
-                for (int j = 1; j <= i; j++) {
-                    System.out.print("* ");
-                }
-             System.out.println(); 
-         }
-        
         sisestus.close();
     }
 
-        //static void u15() {
-        //}
-        //static void u23() {
-        
-        static void u24() {
-            Random rand = new Random();
-            int arv = rand.nextInt(100);
-            int arvamisi = 0;
+    static void u9() {
+        for (int i = 0; i < 66; i++) {
+            System.out.println(i + 1);
+        }
+    }
 
-            Scanner mangukorrad = new Scanner(System.in);
-            System.out.print("Kas soovite mängida 3 korda (1) või lõpmatuseni (2)?:");
-            int korrad = mangukorrad.nextInt();
+    static void u13() {
+        System.out.print("Sisestage kolmnurga kõrgus: ");
+        int korgus = sisestus.nextInt();
 
-            Scanner sisestus = new Scanner(System.in);
-            System.out.print("Arva ära arv 1-100: ");
-            int arvamus = sisestus.nextInt();
-            
-            if (korrad == 1) {
-                while (arvamisi < 3) {
-                    if (arvamus == arv) {
-                        System.out.println("Arvasid ära!");
-                        break;
-                    } else if (arvamus < arv) {
-                        System.out.println("Arv on suurem");
-                        arvamisi++;
-                        System.out.print("Arva uuesti: ");
-                        arvamus = sisestus.nextInt();
-                    } else if (arvamus > arv) {
-                        System.out.println("Arv on väiksem");
-                        arvamisi++;
-                        System.out.print("Arva uuesti: ");
-                        arvamus = sisestus.nextInt();
-                    }
-                }
-        }   else
-                while (arvamisi >= 0) {
-                    if (arvamus == arv) {
-                        System.out.println("Arvasid ära!");
-                        break;
-                }   else if (arvamus < arv) {
-                        System.out.println("Arv on suurem");
-                        arvamisi++;
-                        System.out.print("Arva uuesti: ");
-                        arvamus = sisestus.nextInt();
-                }   else if (arvamus > arv) {
-                        System.out.println("Arv on väiksem");
-                        arvamisi++;
-                        System.out.print("Arva uuesti: ");
-                        arvamus = sisestus.nextInt();
-                }
+        for (int i = 1; i <= korgus; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
             }
-            sisestus.close();
-            mangukorrad.close();
+            System.out.println();
         }
 
-        
+        sisestus.nextLine();
+    }
+
+    public static void u15(String isikukood) {
+        if (isikukood.length() != 11) {
+            System.out.println("Isikukood peab olema 11 numbri pikkune.");
+        } else {
+            char esimeneNumber = isikukood.charAt(0);
+            int sunniaasta;
+            int kuu;
+            int paev;
+
+            if (esimeneNumber == '3' || esimeneNumber == '4') {
+                // Isikukood on enne 2000. aastat
+                sunniaasta = 1900 + Integer.parseInt(isikukood.substring(1, 3));
+                kuu = Integer.parseInt(isikukood.substring(3, 5));
+                paev = Integer.parseInt(isikukood.substring(5, 7));
+            } else {
+                // Isikukood on parast 2000. aastat
+                sunniaasta = 2000 + Integer.parseInt(isikukood.substring(1, 3));
+                kuu = Integer.parseInt(isikukood.substring(3, 5));
+                paev = Integer.parseInt(isikukood.substring(5, 7));
+            }
+
+            char sugum = isikukood.charAt(0);
+            String sugu;
+
+            if (Character.getNumericValue(sugum) % 2 == 0) {
+                sugu = "naine";
+            } else {
+                sugu = "mees";
+            }
+
+            System.out.println("Sugu: " + sugu);
+            System.out.println("Sünniaeg: " + paev + "." + kuu + "." + sunniaasta);
+        }
+    }
+
+    public static void u23() {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int kasutajaVõidud = 0;
+        int arvutiVõidud = 0;
+
+        System.out.println("Täringumäng algab!");
+
+        while (true) {
+            int kasutajaTäring1 = viskaTäring();
+            int kasutajaTäring2 = viskaTäring();
+            int arvutiTäring1 = random.nextInt(6) + 1;
+            int arvutiTäring2 = random.nextInt(6) + 1;
+
+            System.out.println("Sinu visatud täringud: " + kasutajaTäring1 + " ja " + kasutajaTäring2);
+            System.out.println("Arvuti visatud täringud: " + arvutiTäring1 + " ja " + arvutiTäring2);
+
+            int kasutajaSumma = kasutajaTäring1 + kasutajaTäring2;
+            int arvutiSumma = arvutiTäring1 + arvutiTäring2;
+
+            if (kasutajaSumma > arvutiSumma) {
+                System.out.println("Sa võitsid vooru!");
+                kasutajaVõidud++;
+            } else if (arvutiSumma > kasutajaSumma) {
+                System.out.println("Arvuti võitis vooru!");
+                arvutiVõidud++;
+            } else {
+                System.out.println("Viik sel korral!");
+            }
+
+            System.out.println("Hetkelised tulemused: Sinu võidud: " + kasutajaVõidud + ", Arvuti võidud: " + arvutiVõidud);
+            System.out.print("Kas soovid veel mängida? (jah/ei): ");
+            String vastus = scanner.next();
+
+            if (!vastus.equalsIgnoreCase("jah")) {
+                break;
+            }
+        }
+
+        if (kasutajaVõidud > arvutiVõidud) {
+            System.out.println("Sa võitsid mängu!");
+        } else if (arvutiVõidud > kasutajaVõidud) {
+            System.out.println("Arvuti võitis mängu!");
+        } else {
+            System.out.println("Mäng lõppes viigiga!");
+        }
+
+        scanner.close();
+    }
+
+    public static int viskaTäring() {
+        Random random = new Random();
+        return random.nextInt(6) + 1;
+    }
+
+
+    static void u24() {
+        Random rand = new Random();
+        int arv = rand.nextInt(100);
+        int arvamisi = 0;
+
+        System.out.print("Kas soovite mangida 3 korda (1) või lõpmatuseni (2)?:");
+        int korrad = sisestus.nextInt();
+
+        System.out.print("Arva ara arv 1-100: ");
+        int arvamus = sisestus.nextInt();
+
+        if (korrad == 1) {
+            while (arvamisi < 2) {
+                if (arvamus == arv) {
+                    System.out.println("Arvasid ara!");
+                    break;
+                } else if (arvamus < arv) {
+                    System.out.println("Arv on suurem");
+                    arvamisi++;
+                    System.out.print("Arva uuesti: ");
+                    arvamus = sisestus.nextInt();
+                } else if (arvamus > arv) {
+                    System.out.println("Arv on väiksem");
+                    arvamisi++;
+                    System.out.print("Arva uuesti: ");
+                    arvamus = sisestus.nextInt();
+                }
+            }
+        } else
+            while (arvamisi >= 0) {
+                if (arvamus == arv) {
+                    System.out.println("Arvasid ära!");
+                    break;
+                } else if (arvamus < arv) {
+                    System.out.println("Arv on suurem");
+                    arvamisi++;
+                    System.out.print("Arva uuesti: ");
+                    arvamus = sisestus.nextInt();
+                } else if (arvamus > arv) {
+                    System.out.println("Arv on väiksem");
+                    arvamisi++;
+                    System.out.print("Arva uuesti: ");
+                    arvamus = sisestus.nextInt();
+                }
+            }
+    }
 }
-
-
-
-
-
-
-    
-
